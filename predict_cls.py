c1d9cb28228dbedd3b81cd7817147de7dc9cb7ba
yuquanjie <yuquanjie13@gmail.com>
2017-06-14 15:36:58 +0800

    delete a import

--- predict_cls.py
+++ predict_cls.py
@@ -20,7 +20,6 @@ import re
 import numpy as np
 import cv2
 import os
-import h5py
 from PIL import Image, ImageFont, ImageDraw
 import tools.get_data as get_data
 import tools.point_check as point_check
