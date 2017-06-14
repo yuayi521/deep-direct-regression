799f6570dbfaad6acbd4ef12093e8cbc85e78eba
yuquanjie <yuquanjie13@gmail.com>
2017-06-14 17:04:21 +0800

    first commit

---
+++ tools/resizeImage.py
@@ -0,0 +1,11 @@
+"""
+    @author  jasonYu
+    @date    2017/6/14
+    @version created
+    @email   yuquanjie13@gmail.com
+    @descrip resize image from 2400 * 3200 to 1200 * 1600
+"""
+import cv2
+
+if __name__ == '__main__':
+
