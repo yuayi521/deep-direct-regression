66a78361e3397361274d7f343309cd19a7710712
yuquanjie <yuquanjie13@gmail.com>
2017-06-17 17:30:43 +0800

    modify function parameters

--- tools/mydraw.py
+++ tools/mydraw.py
@@ -3,22 +3,39 @@ import cv2
 import numpy as np
 
 
-def draw_rectangle_image(im, roi):
+def draw_rectangle_image(im, top_lef, dow_rig):
     """
-    draw a rectangel on image
     :param im: cv2.imread() function's returned value
                numpy.ndarray
-    :param roi:2-dimension array,
-               1st dimension is x-axis range, from start to end
-               2nd dimension is y-axis range, from start to end
+    :param top_lef:
+    :param dow_rig:
     :return:
     """
     img_draw = Image.fromarray(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(img_draw)
-    draw.polygon([(roi[0][0], roi[1][0]), (roi[0][1], roi[1][0]),
-                  (roi[0][1], roi[1][1]), (roi[0][0], roi[1][1])],
+    top_rig = [dow_rig[0], top_lef[1]]
+    dow_lef = [top_lef[0], dow_rig[1]]
+    draw.polygon([(top_lef[0], top_lef[1]), (top_rig[0], top_rig[1]),
+                  (dow_rig[0], dow_rig[1]), (dow_lef[0], dow_lef[1])],
                  fill='blue', outline='red')
     img_draw = np.array(img_draw)
     im = cv2.cvtColor(img_draw, cv2.COLOR_RGB2BGR)
     cv2.imshow('img', cv2.resize(im, (1000, 1000)))
+    cv2.waitKey(0)
+
+
+def draw_text_on_image(img, point):
+    """
+    draw a point with text
+    :param img:
+    :param point:
+    :return:
+    """
+    img_draw = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img_draw)
+    text = "OOOOOO"
+    draw.text((point[0], point[1]), text, "red")
+    img_draw = np.array(img_draw)
+    img_draw = cv2.cvtColor(img_draw, cv2.COLOR_RGB2BGR)
+    cv2.imshow('img', cv2.resize(img_draw, (1000, 1000)))
     cv2.waitKey(0)
