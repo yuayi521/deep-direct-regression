5ab487976de8acb21ed899bd433337aecbe77945
yuquanjie <yuquanjie13@gmail.com>
2017-06-20 14:30:39 +0800

    a txt file has a corresponding image file, then add into all_imgs

--- tools/get_data.py
+++ tools/get_data.py
@@ -9,7 +9,7 @@ def get_raw_data(input_path):
     """
     process image and txt file, for each iamge file, reading corresponding txt file,
     getting all text region's coordinates and number of text region
-    :param input_path:
+    :param input_path: a directory has text and image 2 directory
     :return: 1) a list, each element is a dictionary
              'imagePath'
              'boxCoord'
@@ -17,8 +17,8 @@ def get_raw_data(input_path):
              2) number of the txt and image file have processed
     """
     # define variable for returning
-    all_imgs = []
-    coords = []
+    all_imgs = []  # a list, each element is a dictionary
+    coords = []  # a list, storing a image's all text region's coordinates which is wise clock
     num_file_txt = 0
     visulise = False
     print('Parsing annotation files')
@@ -26,6 +26,7 @@ def get_raw_data(input_path):
     annots = [os.path.join(annot_path, s) for s in os.listdir(annot_path)]
     box_num = 0
     for annot in annots:
+        print annot
         with open(annot, 'r') as f:
             num_file_txt += 1
             for line in f:
@@ -34,23 +35,23 @@ def get_raw_data(input_path):
                 (x1, y1, x2, y2) = line_split[0:4]
                 (x3, y3, x4, y4) = line_split[4:8]
                 coords.append((x1, y1, x2, y2, x3, y3, x4, y4))
+            txtfilepath = annot
+            # using regular expression, get image file path
             strinfo = re.compile('text')
             annot = strinfo.sub('image', annot)
             strinfo = re.compile('txt')
-            annot = strinfo.sub('jpg', annot)
-            annotation_data = {'imagePath': annot, 'boxCoord': coords, 'boxNum': box_num}
+            imgfilepath = strinfo.sub('jpg', annot)
+            annotation_data = {'imagePath': imgfilepath, 'boxCoord': coords, 'boxNum': box_num}
             box_num = 0
-            all_imgs.append(annotation_data)
-            # it is very important to empty coords
-            # coords = []
+            # image file wheater corresponding to text file, then add
+            if os.path.isfile(imgfilepath) and os.path.isfile(txtfilepath):
+                all_imgs.append(annotation_data)
 
-            if visulise and annotation_data['imagePath'] == '/home/yuquanjie/Documents/' \
-                                                            'icdar2017rctw_train_v1.2/train/part1/image/image_0.jpg':
+            if visulise:
                 img = cv2.imread(annot)
                 img_draw = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
                 draw = ImageDraw.Draw(img_draw)
                 for coord in coords:
-                    print coord
                     draw.polygon([(float(coord[0]), float(coord[1])), (float(coord[2]), float(coord[3])),
                                   (float(coord[4]), float(coord[5])), (float(coord[6]), float(coord[7]))],
                                  outline="red", fill="blue")
@@ -61,3 +62,6 @@ def get_raw_data(input_path):
             # it is very important to empty coords
             coords = []
     return all_imgs, num_file_txt
+
+if __name__ == '__main__':
+    get_raw_data('/home/yuquanjie/Documents/deep-direct-regression/captured_data')
