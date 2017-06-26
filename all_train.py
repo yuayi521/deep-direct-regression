81b94de4e2a880453b0fe126d35a10932dccb40b
yuquanjie <yuquanjie13@gmail.com>
2017-06-26 16:44:25 +0800

    add plot accuracy and loss of classification and regression

--- all_train.py
+++ all_train.py
@@ -18,6 +18,7 @@ import os
 import h5py
 import tensorflow as tf
 import datetime
+import matplotlib.pyplot as plt
 
 sys.path.append('/home/yuquanjie/Documents/deep-direct-regression/tools')
 HUBER_DELTA = 1.0
@@ -65,6 +66,7 @@ def my_hinge(y_true, y_pred):
     sub_2 = y_pred - y_true
     my_hinge_loss = tf.reduce_mean(tf.square(tf.maximum(0.0, sub_1 * sub_2)))
     return my_hinge_loss
+
 
 """
 def weighted_hinge(y_true, y_pred):
@@ -233,11 +235,52 @@ def multi_task(input_tensor):
     ##########################################################################
     # shared layer
     ##########################################################################
-    x_clas = Convolution2D(1, (1, 1), strides=(1, 1), padding='same', name='out_class')(upscore16)
+    x_clas = Convolution2D(1, (1, 1), strides=(1, 1), padding='same', name='cls')(upscore16)
     x = Convolution2D(128, (1, 1), strides=(1, 1), padding='same', activation='relu')(upscore16)
     x = Convolution2D(8, (1, 1), strides=(1, 1), padding='same', activation='sigmoid')(x)
     x_regr = Lambda(lambda t: 800 * t - 400)(x)
     return [x_clas, x_regr, x]
+
+
+def plot_model_history(model_history):
+    fig, axs = plt.subplots(2, 2, figsize=(15, 5))
+    # summarize history for accuracy --- classification
+    axs[0][0].plot(range(1, len(model_history.history['cls_acc']) + 1), model_history.history['cls_acc'])
+    axs[0][0].plot(range(1, len(model_history.history['val_cls_acc']) + 1), model_history.history['val_cls_acc'])
+    axs[0][0].set_title('Model Accuracy Classification')
+    axs[0][0].set_ylabel('Accuracy')
+    axs[0][0].set_xlabel('Epoch')
+    axs[0][0].set_xticks(np.arange(1, len(model_history.history['cls_acc']) + 1), len(model_history.history['cls_acc']) / 10)
+    axs[0][0].legend(['train', 'val'], loc='best')
+
+    # summarize history for accuracy --- Regression
+    axs[0][1].plot(range(1, len(model_history.history['lambda_1_acc']) + 1), model_history.history['lambda_1_acc'])
+    axs[0][1].plot(range(1, len(model_history.history['val_lambda_1_acc']) + 1), model_history.history['val_lambda_1_acc'])
+    axs[0][1].set_title('Model Accuracy Regression')
+    axs[0][1].set_ylabel('Accuracy')
+    axs[0][1].set_xlabel('Epoch')
+    axs[0][1].set_xticks(np.arange(1, len(model_history.history['lambda_1_acc']) + 1), len(model_history.history['lambda_1_acc']) / 10)
+    axs[0][1].legend(['train', 'val'], loc='best')
+
+    # summarize history for loss --- classification
+    axs[1][0].plot(range(1, len(model_history.history['cls_loss']) + 1), model_history.history['cls_loss'])
+    axs[1][0].plot(range(1, len(model_history.history['val_cls_loss']) + 1), model_history.history['val_cls_loss'])
+    axs[1][0].set_title('Model Loss Classification')
+    axs[1][0].set_ylabel('Loss')
+    axs[1][0].set_xlabel('Epoch')
+    axs[1][0].set_xticks(np.arange(1, len(model_history.history['cls_loss']) + 1), len(model_history.history['cls_loss']) / 10)
+    axs[1][0].legend(['train', 'val'], loc='best')
+
+    # summarize history for loss --- Regression
+    axs[1][1].plot(range(1, len(model_history.history['lambda_1_loss']) + 1), model_history.history['lambda_1_loss'])
+    axs[1][1].plot(range(1, len(model_history.history['val_lambda_1_loss']) + 1), model_history.history['val_lambda_1_loss'])
+    axs[1][1].set_title('Model Loss Regression')
+    axs[1][1].set_ylabel('Loss')
+    axs[1][1].set_xlabel('Epoch')
+    axs[1][1].set_xticks(np.arange(1, len(model_history.history['lambda_1_loss']) + 1), len(model_history.history['lambda_1_loss']) / 10)
+    axs[1][1].legend(['train', 'val'], loc='best')
+
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -250,47 +293,28 @@ if __name__ == '__main__':
     sgd = optimizers.SGD(lr=0.01, decay=4e-4, momentum=0.9)
     # compile model
     # multask_model.compile(loss=[my_hinge, smooth_l1], optimizer=sgd)
-    multask_model.compile(loss=[my_hinge, new_smooth], optimizer=sgd)
+    multask_model.compile(loss=[my_hinge, new_smooth], optimizer=sgd, metrics=['acc'])
 
     # resume training
     # model.save_weights() use load_weights()
     # multask_model.load_weights('model/2017-06-23-17-14-loss-decrease-1827-0.65.hdf5')
-    multask_model = load_model('model/2017-06-23-17-14-loss-decrease-1827-0.65.hdf5',
-                               custom_objects={'my_hinge': my_hinge, 'new_smooth': new_smooth})
+
+    # multask_model = load_model('model/2017-06-23-17-14-loss-decrease-1827-0.65.hdf5',
+    #                            custom_objects={'my_hinge': my_hinge, 'new_smooth': new_smooth})
 
     # read training data from h5 file
     print 'reading data from h5 file .....'
     filenamelist = ['dataset/train_size320_1_.h5', 'dataset/train_size320_2_.h5', 'dataset/train_size320_3_.h5']
-    # filenamelist = ['dataset/train_size320_num1500.h5']
     X, Y = read_multi_h5file(filenamelist)
-    print 'traning data, input shape is {0}, output classifiction shape is {1}, regression shape is {2}'.\
+    print 'traning data, input shape is {0}, output classifiction shape is {1}, regression shape is {2}'. \
         format(X.shape, Y[0].shape, Y[1].shape)
-    """"
-    # read validation data from h5 file
-    file_read = h5py.File('dataset/train_size320_num1500.h5', 'r')
-    X_val = file_read['X_train'][:]
-    Y_val_1 = file_read['Y_train_cls'][:]
-    Y_val_2 = file_read['Y_train_merge'][:]
-    Y_val = [Y_val_1, Y_val_2]
-    file_read.close()
-    print 'validation data, input shape is {0}, output shape classification shape is {1}, regression ' \
-          'shape is {2}'.format(X_val.shape, Y_val[0].shape, Y[1].shape)
-    """
     # saved model file path and name
     # get date and time
     date_time = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
     filepath = "model/" + date_time + "-loss-decrease-{epoch:02d}-{loss:.2f}.hdf5"
-    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True,
-                                 mode='min')
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
     # fit model
-    use_val_data = False
-    if use_val_data:
-        # add validation data
-        # loss_class = multask_model.fit(X, Y, batch_size=8, epochs=5000, shuffle=True, callbacks=callbacks_list,
-        #                              validation_data=(X_val, Y_val), verbose=1)
-        print '1'
-    else:
-        # not use validation data for faster speed
-        loss_class = multask_model.fit(X, Y, batch_size=64, epochs=5000, shuffle=True,
-                                       callbacks=callbacks_list, verbose=1)
+    model_info = loss_class = multask_model.fit(X, Y, batch_size=64, epochs=83, shuffle=True,
+                                                callbacks=callbacks_list, verbose=1, validation_split=0.1)
+    plot_model_history(model_info)
