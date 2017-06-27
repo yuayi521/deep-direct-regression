d06fb938a73ddb4a5a6cb20aba6a30f6f3467acf
yuquanjie <yuquanjie13@gmail.com>
2017-06-27 14:25:51 +0800

    init commit

---
+++ tools/move_img_txt.py
@@ -0,0 +1,12 @@
+"""
+    @author  jasonYu
+    @date    2017-06-27
+    @version created
+    @email   yuquanjie13@gmail.com
+    @descrip resized_320 directory has 400k image and txt file, create a directory train_idx,
+             every 4k image and txt file
+"""
+
+
+if __name__ == '__main__':
+    print '1'
