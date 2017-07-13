cc5d9559e2abc00e49a6e9198871bb5af22c9d3a
yuquanjie <yuquanjie13@gmail.com>
2017-07-13 10:57:25 +0800

    add function get_pred_img()

--- predict.py
+++ predict.py
@@ -17,11 +17,10 @@ import numpy as np
 import cv2
 import os
 from PIL import Image, ImageDraw
-import tools.get_data as get_data
+import glob
 import tools.point_check as point_check
 import tools.nms as nms
 import tools.draw_loss as draw_loss
-import re
 
 
 def tf_count(t, val):
@@ -191,107 +190,21 @@ def multi_task(input_tensor=None):
     return [x_clas, x_regr, x]
 
 
-def get_train_data(all_img):
-    visulise = False
-    num_text = 0.0
-    num_no_text = 0.0
+def get_pred_img(all_img):
+    """
+    get image data for predicting
+    :param all_img: a lsit containing all image data whcih need to predict
+    :return: image's numpy array
+             image's path
+    """
     while True:
-        for img_data in all_img:
-            print img_data['imagePath']
-            # image file wheater corresponding to text fle
-            annot = img_data['imagePath']
-            strinfo = re.compile('image/')
-            annot = strinfo.sub('text/', annot)
-            strinfo = re.compile('jpg')
-            annot = strinfo.sub('txt', annot)
-
-            if os.path.isfile(img_data['imagePath']) and os.path.isfile(annot):
-                img = cv2.imread(img_data['imagePath'])
-                width = img.shape[0]  # 3200, shape 0 is height
-                height = img.shape[1]  # 2400, shape 1 is width
-
-                # 1)generate input data
-                # 1.1)input image, from (2400,3200) to (320,320)
-                img_320 = cv2.resize(img, (320, 320), interpolation=cv2.INTER_CUBIC)
-
-                # 2)generate output data
-                # 2.1)generate classification output data
-                divi_x = float(height) / 80.0
-                divi_y = float(width) / 80.0
-                y_class_lable = -1 * np.ones((80, 80))
-                # y_class_lable = np.zeros((80, 80))
-                for ix in xrange(y_class_lable.shape[0]):
-                    for jy in xrange(y_class_lable.shape[1]):
-                        for polygon in img_data['boxCoord']:
-                            x1 = string.atof(polygon[0]) / divi_x
-                            x2 = string.atof(polygon[2]) / divi_x
-                            x3 = string.atof(polygon[4]) / divi_x
-                            x4 = string.atof(polygon[6]) / divi_x
-
-                            y1 = string.atof(polygon[1]) / divi_y
-                            y2 = string.atof(polygon[3]) / divi_y
-                            y3 = string.atof(polygon[5]) / divi_y
-                            y4 = string.atof(polygon[7]) / divi_y
-
-                            polygon = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-                            if point_check.point_in_polygon(ix, jy, polygon):
-                                y_class_lable[ix][jy] = 1
-                                # else:
-                                #    y_class_lable[ix][jy] = 0
-
-                img_320 = np.expand_dims(img_320, axis=0)
-                y_class_lable = np.expand_dims(y_class_lable, axis=0)
-                y_class_lable = np.expand_dims(y_class_lable, axis=3)
-
-                # statistic number of text region and non-text region
-                one_locs = np.where(y_class_lable > 0)
-                # print len(one_locs[0])
-                num_text += len(one_locs[0])
-                num_no_text += 6400 - len(one_locs[0])
-                # print len(one_locs[0]) / (6400.0 - len(one_locs[0]))
-                # 2.2)generate regression output data
-                # y_regr_lable = np.zeros((80,80,8)).astype(np.float32)
-                y_regr_lable = np.zeros((80, 80, 8))
-                for i in xrange(len(one_locs[0])):
-                    # get quadrilateral vertex 4 corrdinates
-                    for polygon in img_data['boxCoord']:
-                        x1 = string.atof(polygon[0]) / divi_x
-                        x2 = string.atof(polygon[2]) / divi_x
-                        x3 = string.atof(polygon[4]) / divi_x
-                        x4 = string.atof(polygon[6]) / divi_x
-
-                        y1 = string.atof(polygon[1]) / divi_y
-                        y2 = string.atof(polygon[3]) / divi_y
-                        y3 = string.atof(polygon[5]) / divi_y
-                        y4 = string.atof(polygon[7]) / divi_y
-
-                        poly = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-                        ix = one_locs[0][i]
-                        jy = one_locs[1][i]
-                        if point_check.point_in_polygon(ix, jy, poly):
-                            left_top_x = poly[0][0]
-                            left_top_y = poly[0][1]
-                            righ_top_x = poly[1][0]
-                            righ_top_y = poly[1][1]
-
-                            righ_dow_x = poly[2][0]
-                            righ_dow_y = poly[2][1]
-                            left_dow_x = poly[3][0]
-                            left_dow_y = poly[3][1]
-
-                            y_regr_lable[ix][jy][0] = left_top_x * 4 - ix * 4
-                            y_regr_lable[ix][jy][1] = left_top_y * 4 - jy * 4
-                            y_regr_lable[ix][jy][2] = righ_top_x * 4 - ix * 4
-                            y_regr_lable[ix][jy][3] = righ_top_y * 4 - jy * 4
-
-                            y_regr_lable[ix][jy][4] = righ_dow_x * 4 - ix * 4
-                            y_regr_lable[ix][jy][5] = righ_dow_y * 4 - jy * 4
-                            y_regr_lable[ix][jy][6] = left_dow_x * 4 - ix * 4
-                            y_regr_lable[ix][jy][7] = left_dow_y * 4 - jy * 4
-                y_regr_lable = np.expand_dims(y_regr_lable, axis=0)
-                yield np.copy(img_320), np.copy(y_class_lable), np.copy(y_regr_lable), np.copy(img), img_data
-            else:
-                continue
+        for img_path in all_img:
+            print img_path
+            if os.path.isfile(img_path):
+                im_arr = cv2.imread(img_path)
+                im_arr
+                img_path_dict = {'imagePath': img_path}
+                yield np.copy(im_arr), img_path_dict
 
 
 if __name__ == '__main__':
@@ -309,15 +222,16 @@ if __name__ == '__main__':
     # multask_model.compile(loss=[my_hinge, smoothL1], optimizer=sgd)
     multask_model.compile(loss=[my_hinge, new_smooth], optimizer=sgd)
     # train data, test model using train data
-    # all_imgs, numFileTxt = get_data.get_raw_data('/home/yuquanjie/Documents/icdar2017rctw_train_v1.2/train/part1')
-    all_imgs, numFileTxt = get_data.get_raw_data('/home/yuquanjie/Documents/shumei_crop_center_test')
-    data_gen_train = get_train_data(all_imgs)
+    all_imgs = glob.glob('/home/yuquanjie/Documents/shumei_crop_center_test/' + '*.jpg')
+    # python generator
+    data_gen_pred = get_pred_img(all_imgs)
     while True:
-        X, Y_cls, Y_regr, raw_img, img_data = data_gen_train.next()
+        X, img_data = data_gen_pred.next()
         # load model
         final_model = load_model('model/2017-07-12-19-03-loss-decrease-65-1.05.hdf5',
                                  custom_objects={'my_hinge': my_hinge, 'new_smooth': new_smooth})
         # predict
+        X = np.expand_dims(X, axis=0)
         predict_all = final_model.predict_on_batch(1/255.0 * X)
         # 1) classification result
         predict_cls = predict_all[0]
